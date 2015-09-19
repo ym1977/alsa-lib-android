69d6604df1bc6d10986ec6a3d59e9d197557cbd9
dashon <dashon@o2fun.com>
2015-09-19 15:41:04 +0800

    prebuild conf files

--- Android.mk
+++ Android.mk
@@ -6,8 +6,50 @@
 #ifeq ($(strip $(BOARD_USES_ALSA_AUDIO)),true)
 
 LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
 
+#####################################################
+##
+## Copy ALSA configuration files to rootfs
+##
+LOCAL_ALSA_CONF_DIR  := $(LOCAL_PATH)/src/conf
+
+alsa_conf_files := \
+	alsa.conf \
+	pcm/dsnoop.conf \
+	pcm/modem.conf \
+	pcm/dpl.conf \
+	pcm/default.conf \
+	pcm/surround51.conf \
+	pcm/surround41.conf \
+	pcm/surround50.conf \
+	pcm/dmix.conf \
+	pcm/center_lfe.conf \
+	pcm/surround40.conf \
+	pcm/side.conf \
+	pcm/iec958.conf \
+	pcm/rear.conf \
+	pcm/surround71.conf \
+	pcm/front.conf \
+	cards/aliases.conf
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := alsa-conf
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_OUT)/usr/share/alsa
+include $(BUILD_PREBUILT)
+
+$(LOCAL_INSTALLED_MODULE):
+	rm -rf $(TARGET_OUT)/usr/share/alsa
+	mkdir -p $(TARGET_OUT)/usr/share/alsa
+	cd $(@D) && tar xvfz $(abspath $<)
+
+$(LOCAL_BUILT_MODULE):
+	mkdir -p $(@D)
+	cd external/airplay/alsa-lib/src/conf && tar cvfz $(abspath $@) $(alsa_conf_files)
+
+#############################################
+include $(CLEAR_VARS)
 LOCAL_MODULE := libasound
 LOCAL_MODULE_TAGS := optional
 
